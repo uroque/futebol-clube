c00b8c64a62c91975ca3755240086cadd5faebec
Ulisses Roque <ulisses.roque@gmail.com>
2022-08-26 19:51:38 -0300

    feat: adds sameTeam method to MatchesController

--- app/backend/src/controllers/Matches.controller.ts
+++ app/backend/src/controllers/Matches.controller.ts
@@ -36,4 +36,19 @@ export default class MatchesController {
       next(err);
     }
   };
+
+  static sameTeam = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { homeTeam, awayTeam } = req.body;
+
+      if (homeTeam === awayTeam) {
+        return res.status(401)
+          .json({ message: 'It is not possible to create a match with two equal teams' });
+      }
+
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
 }
