6d77aca47c21876af460a37706b3605e045cfcf7
Ulisses Roque <ulisses.roque@gmail.com>
2022-08-28 23:57:13 -0300

    chore: adds authToken to create match router

--- app/backend/src/routes/Matches.route.ts
+++ app/backend/src/routes/Matches.route.ts
@@ -1,11 +1,12 @@
 import * as express from 'express';
+import authToken from '../middlewares/authToken';
 import MatchesController from '../controllers/Matches.controller';
 
 const router = express.Router();
 
 router.get('/', MatchesController.getAll);
 
-router.post('/', MatchesController.sameTeam, MatchesController.create);
+router.post('/', authToken, MatchesController.sameTeam, MatchesController.create);
 
 router.patch('/:id/finish', MatchesController.updateProgress);
 
