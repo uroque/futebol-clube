abecac46341f3e0cbebe5f7f23c1ccc61098dc6e
Ulisses Roque <ulisses.roque@gmail.com>
2022-09-18 16:52:29 -0300

    test: adds Login test

---
+++ app/backend/src/tests/Login.test.ts
@@ -0,0 +1,52 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Example from '../database/models/ExampleModel';
+
+import { Response } from 'superagent';
+import { it } from 'mocha';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Login', () => {
+  it('if wrong email or password, should return status 401', async () => {
+    const response = await chai.request(app)
+      .post('/login');
+
+      expect(response.status).to.be.equal(401);
+  })
+  /**
+   * Exemplo do uso de stubs com tipos
+   */
+
+  // let chaiHttpResponse: Response;
+
+  // before(async () => {
+  //   sinon
+  //     .stub(Example, "findOne")
+  //     .resolves({
+  //       ...<Seu mock>
+  //     } as Example);
+  // });
+
+  // after(()=>{
+  //   (Example.findOne as sinon.SinonStub).restore();
+  // })
+
+  // it('...', async () => {
+  //   chaiHttpResponse = await chai
+  //      .request(app)
+  //      ...
+
+  //   expect(...)
+  // });
+
+  it('Seu sub-teste', () => {
+    expect(false).to.be.eq(true);
+  });
+});
