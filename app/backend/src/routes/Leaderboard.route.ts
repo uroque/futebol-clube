9accd95802479d1edccee4e97ef6565f3f7381d0
Ulisses Roque <ulisses.roque@gmail.com>
2022-09-18 17:43:32 -0300

    feat: adds /away to Leaderboard router

--- app/backend/src/routes/Leaderboard.route.ts
+++ app/backend/src/routes/Leaderboard.route.ts
@@ -3,6 +3,8 @@ import LeaderboardController from '../controllers/Leaderboard.controller';
 
 const router = express.Router();
 
-router.get('/home', LeaderboardController.getLeaderboard);
+router.get('/home', LeaderboardController.getLeaderboardHome);
+
+router.get('/away', LeaderboardController.getLeaderboardAway);
 
 export default router;
