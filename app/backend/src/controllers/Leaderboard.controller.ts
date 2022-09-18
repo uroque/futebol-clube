5d9cb12372d9e6609b5a850d07da9b9246df80c5
Ulisses Roque <ulisses.roque@gmail.com>
2022-09-18 16:56:10 -0300

    feat: creates Leaderboard controller

---
+++ app/backend/src/controllers/Leaderboard.controller.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from 'express';
+import LeaderboardService from '../services/Leaderboard.service';
+
+export default class LeaderboardController {
+  static getLeaderboard = async (_req: Request, res: Response, next: NextFunction) => {
+    try {
+      const leaderboard = await LeaderboardService.getLeaderboard();
+
+      return res.status(200).json(leaderboard);
+    } catch (err) {
+      next(err);
+    }
+  };
+}
